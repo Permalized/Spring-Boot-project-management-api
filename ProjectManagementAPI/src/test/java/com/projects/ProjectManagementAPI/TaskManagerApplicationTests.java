40b97d0ef1c46b18cf47c4175000b1321c41d2ee
Permalized <nickosiatr@gmail.com>
2026-02-17 14:25:31 +0200

    Test main class naming change

--- ProjectManagementAPI/src/test/java/com/projects/ProjectManagementAPI/TaskManagerApplicationTests.java
+++ ProjectManagementAPI/src/test/java/com/projects/ProjectManagementAPI/TaskManagerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.projects.taskManager;
+package com.projects.ProjectManagementAPI;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TaskManagerApplicationTests {
+class ProjectManagementAPIApplicationTests {
 
 	@Test
 	void contextLoads() {
